2015cd1074e1244f39d1b3380b60041e68a3f59c
oshri987 <119933474+oshri987@users.noreply.github.com>
2022-12-06 13:16:10 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onGesture(Gesture.Shake, function () {
+    randomnumber = randint(0, 10)
+    if (true) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+let randomnumber = 0
+randomnumber = 0
+basic.showNumber(randomnumber)
+basic.forever(function () {
+	
+})
